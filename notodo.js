a42b9c7e48b2d85b271382a186672308c4aceec3
juho-pentinmikko <juho.pentinmikko@gmail.com>
2014-11-13 11:53:53 +0000

    blaablaa

--- notodo.js
+++ notodo.js
@@ -11,6 +11,7 @@ if (Meteor.isClient) {
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
+      //blaablaablaa blaablaablaa
       Session.set("counter", Session.get("counter") + 1);
     }
   });
