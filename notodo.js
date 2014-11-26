92c04dc9504acf8eea948695d1e3141e8e554f4e
juho-pentinmikko <juho.pentinmikko@gmail.com>
2014-11-26 13:58:39 +0000

    datepickerit toimii, ehkä

--- notodo.js
+++ notodo.js
@@ -1,11 +1,17 @@
 contact = new Mongo.Collection("contact");
 Tasks = new Mongo.Collection("tasks");
-if (Meteor.isClient) {
+
+if (Meteor.isClient) {  
+  Meteor.call("getSessionId", function(err, id) {
+  return console.log(id);
+});
+  
   // counter starts at 0
-  Session.setDefault("counter", 0);
+  //Session.setDefault("counter", 0);
 
   Template.tasks.rendered=function() {
     $('#startTime').datepicker();
+    $('#endTime').datepicker();
   }
   
   Template.showTasks.helpers({
@@ -25,27 +31,26 @@ if (Meteor.isClient) {
   Template.tasks.events({
     'click button': function(event, template) {
       event.preventDefault();
-      
-       /*
-      var recipient = event.target.tasks.value;
-      var difficulty = event.target.tasks.value;
-      var priority = event.target.tasks.value; */
       Tasks.insert({
         task_name: template.find("#task").value,
+        startDate: template.find("#startDate").value,
+        endDate: template.find("#endDate").value,
         recipient: template.find("#recipient").value,
         difficulty: template.find("#difficulty").value,
         priority: template.find("#priority").value
-        /*task_name: task,
-        recipient: recipient,
-        difficulty: difficulty,
-        priority: priority*/
     });
     }
   });
 }
 
 if (Meteor.isServer) {
+  
   Meteor.startup(function () {
     // code to run on server at startup
+    Meteor.methods({
+  getSessionId: function() {
+    return this.connection.id;
+  }
+});
   });
 }
